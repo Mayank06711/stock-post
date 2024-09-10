ea8f4166457a6ea185122ecf21a1910cc8232b30
Mayank Soni <mayank93soni@gmail.com>
2024-09-10 18:32:30 +0530

    add email and password validation

--- src/model/user.model.js
+++ src/model/user.model.js
@@ -3,17 +3,48 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 const userSchema = new Schema(
   {
-    username: { type: String, required: [true,"username is required"], trim: true, toLowerCase: true },
-    email: { type: String, required: [true,"Email is required"], trim: true, unique: true },
-    password: { type: String, required: [true,"Password is required"], minlength: 8 },
+    username: {
+      type: String,
+      required: [true, "username is required"],
+      trim: true,
+      toLowerCase: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      trim: true,
+      unique: true,
+      validate: {
+        validator: function (v) {
+          // Regular expression for email validation
+          return /^(?:[a-zA-Z0-9!#$%&'*+/=?^_`{|}~]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)|[a-zA-Z0-9-]*[a-zA-Z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\\]$/.test(
+            v
+          );
+        },
+        message: (props) => `${props.value} is not a valid email format!`,
+      },
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: 8,
+      validate: {
+        validator: function (v) {
+          // Check if the password contains '@'
+          return /[@#$%]/.test(v);
+        },
+        message:
+          "Password must contain at least one of the following special characters: @, #, $, % ",
+      },
+    },
     bio: { type: String, default: "User" },
     avatar: {
       url: { type: String, required: true },
       public_Id: { type: String, required: true },
     },
     refreshToken: {
-        type: String,
-      },
+      type: String,
+    },
     isActive: { type: Boolean, default: true },
   },
   { timestamps: true }
@@ -38,7 +69,8 @@ userSchema.methods.isPasswordCorrect = async function (password) {
 
 userSchema.methods.generateAccessToken = function () {
   return jwt.sign(
-    { // payload
+    {
+      // payload
       _id: this._id,
       email: this.email,
     },
@@ -62,5 +94,3 @@ userSchema.methods.generateRefreshToken = function () {
 };
 
 export const User = mongoose.model("User", userSchema);
-
-
